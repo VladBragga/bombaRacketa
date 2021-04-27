da2fa5f6e9515af1a09a1c2276f0829cd7f50e49
Владислав <79112212+VladBragga@users.noreply.github.com>
2021-04-27 13:21:01 +0300

    add first task to hw7

--- hw7/main.js
+++ hw7/main.js
@@ -1,5 +1,36 @@
-/* var helper = new PaginationHelper(['a','b','c','d','e','f'], 4);
+function PaginationHelper(collection, itemsPerPage){
+  this.items = collection;
+  this.itemsPerPage = itemsPerPage;
+}
+PaginationHelper.prototype.itemCount = function() {
+  return this.items.length;
+} // возвращает количество элементов во всей коллекции\
 
+PaginationHelper.prototype.pageCount = function() {
+    return Math.ceil(this.itemCount() / this.itemsPerPage);
+} //возвращает количество страниц
+
+PaginationHelper.prototype.pageItemCount = function(pageIndex) {
+ var itemsPerPage = this.itemsPerPage,
+     itemCount = this.itemCount(),
+     pageCount = this.pageCount();
+    if (pageIndex >= pageCount || pageIndex < 0) return -1;
+    return this.items.slice(pageIndex * itemsPerPage, itemCount).splice(0, itemsPerPage).length;
+}  // возвращает количество элементов на текущей странице. page_index отсчитывается от нуля.
+  // этот метод должен возвращать -1 для значений pageIndex, выходящих за пределы допустимого диапазона
+
+PaginationHelper.prototype.pageIndex = function(itemIndex) {
+  if (this.itemCount() === 0 
+      || itemIndex < 0 
+      || itemIndex > this.itemCount())        return -1;
+  if (itemIndex === 0 
+      || itemIndex / this.itemsPerPage === 1) return 0;
+  
+  return Math.floor((itemIndex / this.itemsPerPage)); 
+} // определяет, на какой странице находится элемент. Индексы с нулевым отсчетом
+  // этот метод должен возвращать -1 для значений itemIndex, выходящих за пределы допустимого диапазона
+
+var helper = new PaginationHelper(['a','b','c','d','e','f'], 4);
 helper.pageCount(); //should == 2
 
 helper.itemCount(); //should == 6
@@ -9,15 +40,13 @@ helper.pageItemCount(0); //should == 4
 helper.pageItemCount(1); // last page - should == 2
 
 helper.pageItemCount(2); // should == -1 since the page is invalid
-
-
-// pageIndex takes an item index and returns the page that it belongs on
-
+// pageIndex принимает индекс элемента и возвращает страницу, которой он принадлежит
 helper.pageIndex(5); //should == 1 (zero based index)
 
 helper.pageIndex(2); //should == 0
 
 helper.pageIndex(20); //should == -1
+helper.pageIndex(-10); //should == -1
 
 helper.pageIndex(-10); //should == -1 */
 
